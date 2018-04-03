49686db41d54eaf25c4eb45c5e5ed47a965d4bc1
clerie <clemens@milchinsel.de>
2018-04-03 11:42:35 +0200

    Node.js initialisiert

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 61813;
+
+app.use(express.static(__dirname + '/public'));
+
+/*
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+io.on('connection', onConnection);
+*/
+
+http.listen(port, () => console.log('listening on port ' + port));
